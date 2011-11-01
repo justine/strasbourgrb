78fe9928d85d0fc74df13f5e09499804e048c2d7
Justine Adam <ja@MacBook-Pro-de-ja.local>
2011-11-01 19:17:58 +0100

    Create and clean branch for rails3 version

--- app/models/page.rb
+++
@@ -1,6 +0,0 @@
-class Page < ActiveRecord::Base
-  attr_accessible :title_fr, :title_en, :content_fr, :content_en
-  translatable_columns :title
-  translatable_columns :content
-
-end
--- app/models/user_session.rb
+++
@@ -1,7 +0,0 @@
-class UserSession < Authlogic::Session::Base
-  # use custome find by login method to allow both
-  # login and email to be used for auth
-  find_by_login_method :find_by_login_or_email
-  # custome login fail message
-  generalize_credentials_error_messages "Login failed."
-end
