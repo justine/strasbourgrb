9709760645bbeb4ea699f16dc25d488634093a78
Justine Adam <justine.adam@gmail.com>
2012-02-16 15:12:16 +0100

    Remove project for master branch

--- app/models/project.rb
+++
@@ -1,7 +0,0 @@
-class Project < ActiveRecord::Base
-  attr_accessible :name, :url, :description, :published
-  acts_as_taggable
-  acts_as_textiled :description
-  cattr_reader :per_page
-  @@per_page = 10
-end
